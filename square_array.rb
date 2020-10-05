54f14641a986b4c139fe4fcd3fc31882ae3e8cfd
simonemayers <simone21295@gmail.com>
2020-10-05 16:15:10 -0400

    Done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number| 
+    new_array.push(number * number)
+  end
+  return new_array
 end
+
+# def square_array(array) 
+#   array.collect do |n| 
+#     n*n 
+#   end
+# end
